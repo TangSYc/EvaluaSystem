71dddba05f109b6213c1429b7acdd23a46eacb2b
constantine418 <3296317166@qq.com>
2020-10-27 12:27:54 +0800

    project init

---
+++ app/Login.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Login extends \Illuminate\Foundation\Auth\User implements JWTSubject,Authenticatable
+{
+    protected $table = "login";
+    public $timestamps = true;
+    protected $primaryKey = 'phone';
+    protected $fillable = ['phone', 'password'];
+    protected $hidden = [
+        'password',
+    ];
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    public function getJWTIdentifier()
+    {
+        return self::getKey();
+    }
+
+    /**
+     * 创建用户
+     *
+     * @param array $array
+     * @return |null
+     * @throws \Exception
+     */
+    public static function createUser($array = [])
+    {
+        try {
+            return self::create($array) ?
+                true :
+                false;
+        } catch (\Exception $e) {
+            //\App\Utils\Logs::logError('添加用户失败!', [$e->getMessage()]);
+            die($e->getMessage());
+            return false;
+        }
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::prefix('auth')->namespace('OAuth')->group(function () {
+    Route::post('login', 'AuthController@login'); //登陆
+    Route::post('logout', 'AuthController@logout'); //退出登陆
+    Route::post('refresh', 'AuthController@refresh'); //刷新token
+    Route::post('register', 'AuthController@registered'); //注册
+});
